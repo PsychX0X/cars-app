a3dbb78a1c855e1f5fa3fddf5e55acd0c2a49b4d
PsychX0X <sashko1516@mail.ru>
2023-01-16 04:50:46 +0300

    t

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, HostListener} from '@angular/core';
 import {FormBuilder, Validators} from "@angular/forms";
+import {AppService} from "./app.service";
 
 @Component({
   selector: 'app-root',
@@ -13,52 +14,13 @@ export class AppComponent {
     car: ['', Validators.required],
   })
 
-  carsData = [
-    {
-      image: "1.png",
-      name: "Lamborghini Huracan Spyder",
-      gear: "полный",
-      engine: 5.2,
-      places: 2
-    },
-    {
-      image: "2.png",
-      name: "Chevrolet Corvette",
-      gear: "полный",
-      engine: 6.2,
-      places: 2
-    },
-    {
-      image: "3.png",
-      name: "Ferrari California",
-      gear: "полный",
-      engine: 3.9,
-      places: 4
-    },
-    {
-      image: "4.png",
-      name: "Lamborghini Urus",
-      gear: "полный",
-      engine: 4.0,
-      places: 5
-    },
-    {
-      image: "5.png",
-      name: "Audi R8",
-      gear: "полный",
-      engine: 5.2,
-      places: 2
-    },
-    {
-      image: "6.png",
-      name: "Аренда Chevrolet Camaro",
-      gear: "полный",
-      engine: 2.0,
-      places: 4
-    },
-  ];
+  carsData: any;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private appService: AppService) {
+  }
+
+  ngOnInit() {
+    this.appService.getData().subscribe(carsData => this.carsData = carsData);
   }
 
   goScroll(target: HTMLElement, car?: any) {
@@ -69,12 +31,14 @@ export class AppComponent {
   }
 
   trans: any;
+
   @HostListener('document:mousemove', ['$event'])
   onMouseMove(e: MouseEvent) {
     this.trans = {transform: 'translate3d(' + ((e.clientX * 0.3) / 8) + 'px,' + ((e.clientY * 0.3) / 8) + 'px,0px)'};
   }
 
   bgPos: any;
+
   @HostListener('document:scroll', ['$event'])
   onScroll() {
     this.bgPos = {backgroundPositionX: '0' + (0.3 * window.scrollY) + 'px'};
@@ -82,8 +46,18 @@ export class AppComponent {
 
   onSubmit() {
     if (this.priceForm.valid) {
-      alert("Спасибо за заявку, мы свяжемся с вами в ближайшее время!");
-      this.priceForm.reset();
+      this.appService.sendQuery(this.priceForm.value)
+        .subscribe(
+          {
+            next: (response: any) => {
+              alert(response.message);
+              this.priceForm.reset()
+            },
+            error: (response) => {
+              alert(response.error.message);
+            }
+          }
+        );
     }
   }
 }
